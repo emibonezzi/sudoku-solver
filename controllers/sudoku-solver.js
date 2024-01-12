5936b279733306dfa931add91ab4bedf4d459e8b
emibonezzi <emibonezzi@gmail.com>
2024-01-12 15:03:51 -0500

    added checkRowPlacement function

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,46 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    let sudokuRegex = /^[.1-9]+(?!\D+)[.1-9]+$/;
+    let longEnough = puzzleString.length === 81;
+
+    if (longEnough && sudokuRegex.test(puzzleString)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
+    let rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+    let rowCounter = 0;
+    for (let i = 0; i < puzzleString.length; i++) {
+      // every 9 characters
+      if (i % 9 === 0 && i != 0) {
+        rowCounter++;
+      }
 
+
+      if (row === rows[rowCounter]) {
+        if (puzzleString[i] === value) {
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  }
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+  solve(puzzleString) {}
 }
 
+let solver = new SudokuSolver();
+let result = solver.checkRowPlacement("1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.", "A", "4", "5")
+console.log(result)
+
 module.exports = SudokuSolver;
+
 
