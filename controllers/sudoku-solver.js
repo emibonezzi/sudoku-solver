7aed2560a02d9d6f38864f6114c89de627cd6d05
emibonezzi <emibonezzi@gmail.com>
2024-01-15 19:09:21 -0500

    add solve function, bugged

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -48,8 +48,16 @@ class SudokuSolver {
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-    let rowsRegions = [["A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"]];
-    let colsRegions = [["1","2","3"], ["4","5","6"], ["7","8","9"]];
+    let rowsRegions = [
+      ["A", "B", "C"],
+      ["D", "E", "F"],
+      ["G", "H", "I"],
+    ];
+    let colsRegions = [
+      ["1", "2", "3"],
+      ["4", "5", "6"],
+      ["7", "8", "9"],
+    ];
     let colsRegionsCounter = 0;
     let rowsRegionsCounter = 0;
 
@@ -63,7 +71,10 @@ class SudokuSolver {
       if (i % 9 === 0 && i != 0) {
         colsRegionsCounter = 0;
       }
-      if (colsRegions[colsRegionsCounter].includes(column) && rowsRegions[rowsRegionsCounter].includes(row)) {
+      if (
+        colsRegions[colsRegionsCounter].includes(column) &&
+        rowsRegions[rowsRegionsCounter].includes(row)
+      ) {
         if (value === puzzleString[i]) {
           return false;
         }
@@ -73,15 +84,71 @@ class SudokuSolver {
     return true;
   }
 
-  solve(puzzleString) {}
+  solve(puzzleString) {
+    let guesses = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    let rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+    let puzzleArr = puzzleString.split("")
+
+    while (puzzleArr.includes(".")) {
+      let rowCounter = 0;
+      let colCounter = 1;
+      for (let i = 0; i < puzzleArr.length; i++) {
+        if (i % 9 === 0 && i != 0) {
+          rowCounter++;
+        }
+
+        if (colCounter > 9) {
+          colCounter = 1;
+        }
+
+        if (puzzleArr[i] === ".") {
+          let solutions = 0;
+          console.log(rows[rowCounter], colCounter);
+          for (let n = 0; n < guesses.length; n++) {
+            console.log("guessing", guesses[n]);
+            if (
+              this.checkColPlacement(
+                puzzleString,
+                rows[rowCounter],
+                colCounter,
+                guesses[n]
+              ) &&
+              this.checkRowPlacement(
+                puzzleString,
+                rows[rowCounter],
+                colCounter,
+                guesses[n]
+              ) &&
+              this.checkRegionPlacement(
+                puzzleString,
+                rows[rowCounter],
+                colCounter,
+                guesses[n]
+              )
+            ) {
+              console.log("here!")
+              solutions++;
+
+              if (solutions > 1) {
+                puzzleArr[i] = ".";
+              } else {
+                puzzleArr[i] = guesses[n];
+              }
+            }
+          }
+        }
+
+        colCounter++;
+      }
+    }
+
+    return puzzleArr.join("");
+  }
 }
 
 let solver = new SudokuSolver();
-let result = solver.checkRegionPlacement(
-  "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
-  "D",
-  "1",
-  "6"
+let result = solver.solve(
+  "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37."
 );
 console.log(result);
 
