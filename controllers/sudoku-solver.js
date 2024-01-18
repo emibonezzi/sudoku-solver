a1eb3e01d325de04321c97b18e57a9688f28a836
emibonezzi <emibonezzi@gmail.com>
2024-01-18 16:47:24 -0500

    added check route

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -13,6 +13,12 @@ class SudokuSolver {
   checkRowPlacement(puzzleString, row, column, value) {
     let rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
     let rowCounter = 0;
+
+    if (!rows.includes(row)) {
+      console.log("INVALID ROW")
+      return false;
+    }
+
     for (let i = 0; i < puzzleString.length; i++) {
       // every 9 characters
       if (i % 9 === 0 && i != 0) {
@@ -31,6 +37,12 @@ class SudokuSolver {
 
   checkColPlacement(puzzleString, row, column, value) {
     let colCounter = 1;
+
+    if (!["1","2","3","4","5","6","7","8","9"].includes(column)) {
+      console.log("INVALID COLUMN")
+      return false;
+    }
+
     for (let i = 0; i < puzzleString.length; i++) {
       if (i % 9 === 0 && i != 0) {
         colCounter = 1;
@@ -89,8 +101,7 @@ class SudokuSolver {
     // CHECK IF PUZZLE STRING IS SOLVED OR NOT
     if (!puzzleString.includes(".")) {
       console.log("solved");
-      let solution =
-        "568913724342687519197254386685479231219538467734162895926345178473891652851726943";
+      let solution = "568913724342687519197254386685479231219538467734162895926345178473891652851726943";
       console.log(puzzleString);
       console.log(puzzleString === solution);
       return true;
@@ -163,7 +174,12 @@ class SudokuSolver {
         }
 
         if (solArr.length === 0) {
-          console.log("Puzzle unsolvable because", row, colCounter, "has no possible candidates.");
+          console.log(
+            "Puzzle unsolvable because",
+            row,
+            colCounter,
+            "has no possible candidates."
+          );
           return false;
         }
       }
@@ -173,7 +189,7 @@ class SudokuSolver {
 
     // CREATE TEMP SOLUTION STRING
     solutionStr = puzzleArr.join("");
-    console.log(solutionsObj);
+    // console.log(solutionsObj);
 
     // CHECK IF THERE'S AT LEAST ONE CELL WITH JUST ONE POSSIBLE ENTRY
     let uniqueSolutions =
@@ -182,22 +198,24 @@ class SudokuSolver {
       ).length > 0;
 
     if (uniqueSolutions) {
-      console.log(
+      /* console.log(
         "At least one cell with one unique solution ->",
         uniqueSolutions
-      );
+      ); */
 
       // CALL THE FUNCTION INSIDE THE FUNCTION PASSING THE TEMP SOLUTION STRING
       this.solve(solutionStr);
     } else {
       console.log("This Sudoku doesn't have one unique solution.");
+      return false;
+      
     }
   }
 }
 
 let solver = new SudokuSolver();
 solver.solve(
-  "..9.287..8.6..4..5..3.....46.........2.71345.........23.....5..9..4..8.7..125.3..",
+  "..9.7...5..21..9..1...28....7...5..1..851.....5....3.......3..68........21.....87",
   "A",
   "9",
   "8"
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,27 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {
+    let row = req.body.coordinate[0].toUpperCase() || false;
+    let column = req.body.coordinate[1] || false;
+    let value = req.body.value || false;
+    let puzzle = req.body.puzzle;
+    if (row && column && value) {
+      if (
+        solver.checkColPlacement(puzzle, row, column, value) &&
+        solver.checkRowPlacement(puzzle, row, column, value) &&
+        solver.checkRegionPlacement(puzzle, row, column, value)
+      ) {
+        res.json({valid: true})
+      } else {
+        res.json({error: "conflict in region, row, column"})
+      }
+    }
+  });
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
-
-    });
+  app.route("/api/solve").post((req, res) => {});
 };
